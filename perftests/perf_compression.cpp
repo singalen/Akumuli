aeda798bb43e53a35a217f6306111c4cace28d92
Eugene Lazin <4lazin@gmail.com>
2016-01-26 19:47:38 +0300

    Compression perf test improved

--- perftests/perf_compression.cpp
+++ perftests/perf_compression.cpp
@@ -1,5 +1,6 @@
 #include "compression.h"
 #include "perftest_tools.h"
+#include "datetime.h"
 
 #include <iostream>
 #include <cstdlib>
@@ -7,7 +8,12 @@
 #include <zlib.h>
 #include <cstring>
 
+#include <sstream>
+#include <fstream>
+#include <boost/filesystem.hpp>
+
 using namespace Akumuli;
+namespace fs = boost::filesystem;
 
 //! Generate time-series from random walk
 struct RandomWalk {
@@ -35,54 +41,91 @@ struct RandomWalk {
     }
 };
 
-int main(int argc, char** argv) {
-    const uint64_t N_TIMESTAMPS = 1000;
-    const uint64_t N_PARAMS = 100;
-    UncompressedChunk header;
-    std::cout << "Testing timestamp sequence" << std::endl;
-    int c = 100;
-    std::vector<aku_ParamId> ids;
-    for (uint64_t id = 0; id < N_PARAMS; id++) { ids.push_back(id); }
-    RandomWalk rwalk(10.0, 0.0, 0.01, N_PARAMS);
-    for (uint64_t id = 0; id < N_PARAMS; id++) {
-        for (uint64_t ts = 0; ts < N_TIMESTAMPS; ts++) {
-            header.paramids.push_back(ids[id]);
-            int k = rand() % 2;
-            if (k) {
-                c++;
-            }
-            header.timestamps.push_back((ts + c) << 8);
-            header.values.push_back(rwalk.generate(0));
+
+UncompressedChunk read_data(fs::path path) {
+    UncompressedChunk res;
+    std::fstream in(path.c_str());
+    std::string line;
+    aku_ParamId base_pid = 1;
+    std::map<std::string, aku_ParamId> pid_map;
+    while(std::getline(in, line)) {
+        std::istringstream lstr(line);
+        std::string series, timestamp, value;
+        std::getline(lstr, series, ',');
+        std::getline(lstr, timestamp, ',');
+        std::getline(lstr, value, ',');
+
+        aku_ParamId id;
+        auto pid_it = pid_map.find(series);
+        if (pid_it == pid_map.end()) {
+            pid_map[series] = base_pid;
+            id = base_pid;
+            base_pid++;
+        } else {
+            id = pid_it->second;
         }
-        c = 100;
+        res.paramids.push_back(id);
+
+        res.timestamps.push_back(DateTimeUtil::from_iso_string(timestamp.c_str()));
+
+        res.values.push_back(std::stod(value));
+
+    }
+    return res;
+}
+
+struct Writer : ChunkWriter {
+    ByteVector *out;
+    Writer(ByteVector *out) : out(out) {}
+
+    virtual aku_MemRange allocate() {
+        aku_MemRange range = {
+            out->data(),
+            static_cast<uint32_t>(out->size())
+        };
+        return range;
     }
 
-    ByteVector out;
-    out.resize(N_PARAMS*N_TIMESTAMPS*24);
+    //! Commit changes
+    virtual aku_Status commit(size_t bytes_written) {
+        out->resize(bytes_written);
+        return AKU_SUCCESS;
+    }
+};
+
+struct TestRunResults {
+    // Akumuli stats
+    std::string file_name;
+    size_t uncompressed;
+    size_t compressed;
+    size_t nelements;
+
+    double bytes_per_element;
+    double compression_ratio;
+
+    // Gzip stats
+    double gz_bytes_per_element;
+    double gz_compression_ratio;
+    double gz_compressed;
+
+    // Performance
+    std::vector<double> perf;
+    std::vector<double> gz_perf;
+};
+
+TestRunResults run_tests(fs::path path, bool bench) {
+    TestRunResults runresults;
+    runresults.file_name = fs::basename(path);
+
+    auto header = read_data(path);
 
     const size_t UNCOMPRESSED_SIZE = header.paramids.size()*8    // Didn't count lengths and offsets
                                    + header.timestamps.size()*8  // because because this arrays contains
                                    + header.values.size()*8;     // no information and should be compressed
                                                                  // to a few bytes
+    ByteVector out;
+    out.resize(UNCOMPRESSED_SIZE * 4);
 
-    struct Writer : ChunkWriter {
-        ByteVector *out;
-        Writer(ByteVector *out) : out(out) {}
-
-        virtual aku_MemRange allocate() {
-            aku_MemRange range = {
-                out->data(),
-                static_cast<uint32_t>(out->size())
-            };
-            return range;
-        }
-
-        //! Commit changes
-        virtual aku_Status commit(size_t bytes_written) {
-            out->resize(bytes_written);
-            return AKU_SUCCESS;
-        }
-    };
     Writer writer(&out);
 
     aku_Timestamp tsbegin, tsend;
@@ -90,7 +133,7 @@ int main(int argc, char** argv) {
     auto status = CompressionUtil::encode_chunk(&n, &tsbegin, &tsend, &writer, header);
     if (status != AKU_SUCCESS) {
         std::cout << "Encoding error" << std::endl;
-        return 1;
+        exit(1);
     }
 
     // Compress using zlib
@@ -105,7 +148,7 @@ int main(int argc, char** argv) {
     Bytef* pgz_val = (Bytef*)aligned_alloc(64, header.values.size()*8);
     memcpy(pgz_val, header.values.data(), header.values.size()*8);
 
-    const auto gz_max_size = N_PARAMS*N_TIMESTAMPS*24;
+    const auto gz_max_size = UNCOMPRESSED_SIZE*10;
     Bytef* pgzout = (Bytef*)aligned_alloc(64, gz_max_size);
     uLongf gzoutlen = gz_max_size;
     size_t total_gz_size = 0, id_gz_size = 0, ts_gz_size = 0, float_gz_size = 0;
@@ -144,20 +187,17 @@ int main(int argc, char** argv) {
     const float BYTES_PER_EL = float(COMPRESSED_SIZE)/header.paramids.size();
     const float COMPRESSION_RATIO = float(UNCOMPRESSED_SIZE)/COMPRESSED_SIZE;
 
-    std::cout << "Uncompressed: " << UNCOMPRESSED_SIZE       << std::endl
-              << "  compressed: " << COMPRESSED_SIZE         << std::endl
-              << "    elements: " << header.paramids.size()  << std::endl
-              << "  bytes/elem: " << BYTES_PER_EL            << std::endl
-              << "       ratio: " << COMPRESSION_RATIO       << std::endl
-    ;
+    // Save compression stats
 
-    std::cout << "Gzip stats: " << std::endl
-              << "bytes/elem: " << GZ_BPE << std::endl
-              << "     ratio: " << GZ_RATIO << std::endl
-              << "  id bytes: " << id_gz_size << std::endl
-              << "  ts bytes: " << ts_gz_size << std::endl
-              << " val bytes: " << float_gz_size << std::endl;
+    runresults.uncompressed         = UNCOMPRESSED_SIZE;
+    runresults.compressed           = COMPRESSED_SIZE;
+    runresults.nelements            = header.timestamps.size();
+    runresults.bytes_per_element    = BYTES_PER_EL;
+    runresults.compression_ratio    = COMPRESSION_RATIO;
 
+    runresults.gz_compressed        = id_gz_size + ts_gz_size + float_gz_size;
+    runresults.gz_compression_ratio = GZ_RATIO;
+    runresults.gz_bytes_per_element = GZ_BPE;
 
     // Try to decompress
     UncompressedChunk decomp;
@@ -184,30 +224,29 @@ int main(int argc, char** argv) {
         }
     }
 
-    if (argc == 2 && std::string(argv[1]) == "benchmark") {
+    if (bench) {
         // Bench compression process
-        const int NRUNS = 1000;
-        PerfTimer tm;
+        const int NRUNS = 100;
         aku_Status tstatus;
-        volatile uint32_t vn;
         ByteVector vec;
+        runresults.perf.reserve(NRUNS);
+        runresults.gz_perf.reserve(NRUNS);
         for (int i = 0; i < NRUNS; i++) {
-            vec.resize(N_PARAMS*N_TIMESTAMPS*24);
+            vec.resize(UNCOMPRESSED_SIZE);
+            PerfTimer tm;
             Writer w(&vec);
             aku_Timestamp ts;
             uint32_t n;
             tstatus = CompressionUtil::encode_chunk(&n, &ts, &ts, &w, header);
             if (tstatus != AKU_SUCCESS) {
                 std::cout << "Encoding error" << std::endl;
-                return 1;
+                exit(1);
             }
-            vn = n;
+            runresults.perf.push_back(tm.elapsed());
         }
-        double elapsed = tm.elapsed();
-        std::cout << "Elapsed (akumuli): " << elapsed << " " << vn << std::endl;
 
-        tm.restart();
         for (int i = 0; i < NRUNS; i++) {
+            PerfTimer tm;
             uLongf offset = 0;
             // compress param ids
             auto zstatus = compress(pgzout, &gzoutlen, pgz_ids, header.paramids.size()*8);
@@ -231,8 +270,61 @@ int main(int argc, char** argv) {
                 std::cout << "GZip error" << std::endl;
                 exit(zstatus);
             }
+            runresults.gz_perf.push_back(tm.elapsed());
         }
-        elapsed = tm.elapsed();
-        std::cout << "Elapsed (zlib): " << elapsed << " " << vn << std::endl;
+    }
+    return runresults;
+}
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        std::cout << "Path to dataset required" << std::endl;
+        exit(1);
+    }
+
+    bool benchmark = false;
+
+    if (argc == 3 && std::string(argv[2]) == "bench") {
+        benchmark = true;
+    }
+
+    // Iter directory
+    fs::path dir{argv[1]};
+    fs::directory_iterator begin(dir), end;
+    std::vector<fs::path> files;
+    std::list<TestRunResults> results;
+    for (auto it = begin; it != end; it++) {
+        files.push_back(*it);
+    }
+    std::sort(files.begin(), files.end());
+    for (auto fname: files) {
+        std::cout << "Run tests for " << fs::basename(fname) << std::endl;
+        results.push_back(run_tests(fname, benchmark));
+    }
+
+    // Write table
+    std::cout << "| File name | num elements | uncompressed | compressed | gzip compressed | ratio " <<
+                 "| gzip ratio | bytes/el | gzip bytes/el | time (s) | gzip time (s) | speedup | compression factor (aku/gzip) " << std::endl;
+    std::cout << "| ----- | ---- | ----- | ---- | ----- | ---- | ----- | ----- | ----- | ----- | ----- | ----- | -----| " << std::endl;
+    for (auto const& run: results) {
+        double maxdouble = std::numeric_limits<double>::max();
+        auto perf = std::accumulate(run.perf.begin(), run.perf.end(), maxdouble, [](double a, double b) { return std::min(a, b); });
+        auto gz_perf = std::accumulate(run.gz_perf.begin(), run.gz_perf.end(), maxdouble, [](double a, double b) { return std::min(a, b); });
+        std::cout << run.file_name << " | " <<
+                     run.nelements << " | " <<
+                     run.uncompressed << " | " <<
+                     run.compressed << " | " <<
+                     run.gz_compressed << " | " <<
+                     run.compression_ratio << " | " <<
+                     run.gz_compression_ratio << " | " <<
+                     run.bytes_per_element << " | " <<
+                     run.gz_bytes_per_element << " | " <<
+                     perf << " | " <<
+                     gz_perf << " | " <<
+                     (gz_perf/perf) << " | " <<
+                     (double(run.compressed)/run.gz_compressed) << " | " <<
+                     std::endl;
+
     }
 }
